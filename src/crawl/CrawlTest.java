e4aec98b02ba7ad761a779f6290e4eda7c4c3600
ScottMcCarthy <ScottMcCarthy@ProQuest.co.uk>
2014-08-18 16:09:52 +0100

    Crawl tester now actaully crawls.

--- src/crawl/CrawlTest.java
+++ src/crawl/CrawlTest.java
@@ -1,19 +1,23 @@
 package crawl;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import org.apache.commons.io.FileUtils;
 
 public class CrawlTest {
 	
 	private static final String crawlURL = "http://10.241.16.212:4200/tdm/sm/37CF5C5524E0EC0FDB60924B2DAE941B/sitemap.index";
+	private static final String locationTag = "loc";
 
 	private static String readExternalFile(URL url){
 		//Check if we need to do a redirect to a different URL.
-		System.out.println("Return code is: "+httpReturnCode(url));
-		
 		String urlOutput = "";
 		
 		if (307 == httpReturnCode(url)){
@@ -64,17 +68,88 @@ public class CrawlTest {
 		return code;
 	}
 	
+	private static List<String> getListOfURLs(String source, String tag){
+		List<String> URLs = new ArrayList<String>();
+		String sourceTemp = source;
+		String startTag = "<" + tag + ">";
+		String endTag = "</" + tag + ">";
+		while (sourceTemp.contains(startTag)){
+			int start = sourceTemp.indexOf(startTag);
+			int end = sourceTemp.indexOf(endTag);
+			String url = sourceTemp.substring(start+startTag.length(), end);
+			sourceTemp = sourceTemp.substring(end+endTag.length());
+			URLs.add(url.replace("&amp;", "&"));
+		}
+		return URLs;
+	}
 	
-	public static void main(String[] args) {
-		System.out.println("About to crawl: "+crawlURL);
-		String customerIndex = "";
+	private static List<String> covertURLsForDevSystem(List<String> urls) {
+		String beforeURL = "nightlyservices.aa1.pqe";
+		String afterURL = "10.241.16.212:4200";
+		ArrayList<String> updatedURLs = new ArrayList<String>();
+		
+		for (String s : urls){
+			updatedURLs.add(s.replaceAll(beforeURL, afterURL));
+		}
+		return updatedURLs;
+	}
+	
+	private static List<String> getURLsFromHyperLink(String url){
+		String output = "";
 		try {
-			customerIndex = readExternalFile(new URL(crawlURL));
+			output = readExternalFile(new URL(url));
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-		System.out.println("Customer index: "+customerIndex);
+		List<String> siteMaps = getListOfURLs(output, locationTag);
+		return siteMaps;
+	}
+	
+	public static void main(String[] args) {
+		//Step 1 : read the customer's Index.
+		int fileNumber = 0;
+		System.out.println("About to crawl: "+crawlURL);
+		List<String> siteMaps = getURLsFromHyperLink(crawlURL);
+		
+		//Only run this stage to spoof URLs for testing on development system.
+		siteMaps = covertURLsForDevSystem(siteMaps);
+		System.out.println("Sitemaps: "+siteMaps);
+		
+		//Step 2 : Read all the sitemaps.
+		for (String siteMap : siteMaps){
+			List<String> dataFileList = covertURLsForDevSystem(getURLsFromHyperLink(siteMap));
+			System.out.println("dataFileList: "+dataFileList);
+			
+			//Step 3 : download the files
+			for (String fileURL : dataFileList) {
+				fileNumber++;
+				final String threadURL = fileURL;
+				final int threadFileNumber = fileNumber;
+					
+					new Thread(new Runnable() {
+					    public void run() {
+					    	try {
+					    		if (307 == httpReturnCode(new URL(threadURL))){
+					    			try {
+					    				FileUtils.copyURLToFile(new URL(get307RedirectURL(new URL(threadURL))), new File("/Users/ScottMcCarthy/crawl/"+threadFileNumber+".xml"));
+					    				return;
 
+					    			} catch (MalformedURLException e) {
+					    				e.printStackTrace();
+					    			}
+					    			
+					    		} else {
+					    			FileUtils.copyURLToFile(new URL(threadURL), new File("/Users/ScottMcCarthy/crawl/"+threadFileNumber+".xml"));
+					    		}
+							} catch (MalformedURLException e) {
+								e.printStackTrace();
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+					    }
+					}).start();
+			}
+		}
 	}
 
 }
