9ea18abc77ee0d445d8153b38db6a554534ee0b2
ScottMcCarthy <ScottMcCarthy@ProQuest.co.uk>
2014-10-01 12:10:43 +0100

    Updating crawl testing tool.

--- src/crawl/CrawlTest.java
+++ src/crawl/CrawlTest.java
@@ -1,16 +1,30 @@
 package crawl;
 
 import java.io.File;
+import java.io.InputStream;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
+/**
+ * On Windows:
+ *  To compile:
+ *  C:\Morningstar\services\Svcs-TdmCrawl\src\main\scripts\CrawlTester\crawl>javac -cp ../commons-io-2.4.jar CrawlTest.java
+ *  To run:
+ *  C:\Morningstar\services\Svcs-TdmCrawl\src\main\scripts\CrawlTester>java -cp commons-io-2.4.jar;. crawl.CrawlTest http://internal-tdm-devl2-1759524920.us-east-1.elb.amazonaws.com/tdm/sm/37CF5C5524E0EC0FDB60924B2DAE941B/sitemap.index C:\temp15
+ */
 public class CrawlTest {
 	
 	//private static final String defaultCrawlURL = "http://10.241.16.212:4200/tdm/sm/37CF5C5524E0EC0FDB60924B2DAE941B/sitemap.index";
@@ -19,6 +33,10 @@ public class CrawlTest {
 	
 	private static String crawlURL = "";
 	private static String destinationDirectory = "";
+	private static int successfullDownloads = 0;
+	
+	private static final ExecutorService executor = Executors.newFixedThreadPool(20);
+	private static final long SHUTDOWN_TIME_SECONDS = 2;
 
 	private static String readExternalFile(URL url){
 		//Check if we need to do a redirect to a different URL.
@@ -43,6 +61,10 @@ public class CrawlTest {
 			}
 		}
 		return urlOutput;
+	}
+	
+	public static void incrementFilesDownloaded() {
+		successfullDownloads++;
 	}
 	
 	private static String get307RedirectURL(URL url){
@@ -109,6 +131,42 @@ public class CrawlTest {
 		return siteMaps;
 	}
 
+	private static void copyURLToFile(URL source, File destination) throws IOException {
+		HttpURLConnection connection = (HttpURLConnection) source.openConnection();
+		connection.setInstanceFollowRedirects(true);
+		InputStream input = connection.getInputStream();
+		// Redirect does not work when switching protocols from http to https, see
+		// http://stackoverflow.com/a/1884427/152233
+		StringWriter writer = new StringWriter();
+		IOUtils.copy(input, writer, StandardCharsets.UTF_8);
+		HttpURLConnection httpsConnection = (HttpURLConnection) new URL(writer.toString()).openConnection();
+		httpsConnection.setInstanceFollowRedirects(true);
+		InputStream httpsInput = httpsConnection.getInputStream();
+		FileUtils.copyInputStreamToFile(httpsInput, destination);
+	}
+	
+	/**
+	 * This method uses the recommended shutdown strategy as documented in {@link ExecutorService}.
+	 */
+	private static void shutdownExecutor() {
+		executor.shutdown(); // Disable new tasks from being submitted
+		try {
+			// Wait a while for existing tasks to terminate
+			if (!executor.awaitTermination(SHUTDOWN_TIME_SECONDS, TimeUnit.SECONDS)) {
+				executor.shutdownNow(); // Cancel currently executing tasks
+				// Wait a while for tasks to respond to being cancelled
+				if (!executor.awaitTermination(SHUTDOWN_TIME_SECONDS, TimeUnit.SECONDS)) {
+					System.err.println("The executor did not terminate after " + SHUTDOWN_TIME_SECONDS + TimeUnit.SECONDS);
+				}
+			}
+		} catch (InterruptedException e) {
+			// (Re-)Cancel if current thread also interrupted
+			executor.shutdownNow();
+			// Preserve interrupt status
+			Thread.currentThread().interrupt();
+		}
+	}
+	
 	public static void main(String[] args) {
 				
 		if (args.length!=2) {
@@ -131,43 +189,51 @@ public class CrawlTest {
 		
 		//Only run this stage to spoof URLs for testing on development system.
 		siteMaps = covertURLsForDevSystem(siteMaps);
-		System.out.println("Sitemaps: "+siteMaps);
+		System.out.println("Found "+siteMaps.size()+" site maps");
 		
 		//Step 2 : Read all the sitemaps.
+		int sitemapCount = 0;
 		for (String siteMap : siteMaps){
+			sitemapCount++;
 			List<String> dataFileList = covertURLsForDevSystem(getURLsFromHyperLink(siteMap));
-			System.out.println("dataFileList: "+dataFileList);
+			System.out.println("Sitemap "+sitemapCount+ "contains "+dataFileList.size()+ "files");
+			int outputProgressEveryXfiles = 10;
+			int progress = 0;
+			
 			
 			//Step 3 : download the files
 			for (String fileURL : dataFileList) {
 				fileNumber++;
+				progress++;
+				if (progress == outputProgressEveryXfiles) {
+					System.out.println("Downloaded "+fileNumber+ "files");
+				}
 				final String threadURL = fileURL;
 				final int threadFileNumber = fileNumber;
 					
-					new Thread(new Runnable() {
-					    public void run() {
-					    	try {
-					    		if (307 == httpReturnCode(new URL(threadURL))){
-					    			try {
-					    				FileUtils.copyURLToFile(new URL(get307RedirectURL(new URL(threadURL))), new File(destinationDirectory+"/"+threadFileNumber+".xml"));
-					    				return;
+				executor.execute(new Runnable() {
+				    @Override
+					public void run() {
+				    	try {
+				    		copyURLToFile(new URL(threadURL), new File(destinationDirectory+"/"+threadFileNumber+".xml"));
+				    		CrawlTest.incrementFilesDownloaded();
+						} catch (MalformedURLException e) {
+							e.printStackTrace();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+				    }
+				});
 
-					    			} catch (MalformedURLException e) {
-					    				e.printStackTrace();
-					    			}
-					    			
-					    		} else {
-					    			FileUtils.copyURLToFile(new URL(threadURL), new File(destinationDirectory+"/"+threadFileNumber+".xml"));
-					    		}
-							} catch (MalformedURLException e) {
-								e.printStackTrace();
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-					    }
-					}).start();
 			}
+			
+			//Output Summary
+			System.out.println();
+			System.out.println("CRAWL COMPLETE");
+			System.out.println("--------------");
+			System.out.println(successfullDownloads + " files downloaded");
 		}
+		shutdownExecutor();
 	}
 
 }
