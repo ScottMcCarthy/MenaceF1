14ff24e746863c311006ef50854562020ea54420
Scott McCarthy <Scott.McCarthy@ProQuest.com>
2019-12-19 14:24:15 +0000

    Adding massive XML document for formatting

---
+++ src/crawl/BrowseGen.java
@@ -0,0 +1,11 @@
+package crawl;
+
+public class BrowseGen {
+	
+	public static void main(String[] args) {
+	for (int x=0;x<2272;x++) {
+		System.out.println("eebo.tt.vol"+x+".subject = Volume "+x);
+	}
+	}
+
+}
