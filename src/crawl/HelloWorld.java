043947e6d65a515f4669c90bbb827ccbf761437a
ScottMcCarthy <Scott.McCarthy@ProQuest.com>
2020-07-13 16:11:52 +0100

    adding directory comparisson

---
+++ src/crawl/HelloWorld.java
@@ -0,0 +1,14 @@
+package crawl;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		int count = 1;
+		while (count<1000000 ) {
+			System.out.println("Hello Scott");
+			count = count+1;
+		}
+	}
+
+}
